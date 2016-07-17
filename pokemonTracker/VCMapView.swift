8afc0245586f50d054885391a3d2f8872f3cb887
Luke Murray <lmurray.nyc@gmail.com>
2016-07-16 20:58:06 -0400

    added location tracking

--- pokemonTracker/VCMapView.swift
+++ pokemonTracker/VCMapView.swift
@@ -12,26 +12,38 @@ import Foundation
 import MapKit
 
 extension ViewController: MKMapViewDelegate {
-    
 
+    func mapView(mapView: MKMapView, didUpdateUserLocation
+        userLocation: MKUserLocation) {
+        var region : MKCoordinateRegion = mapView.region
+        region.center.latitude = userLocation.coordinate.latitude;
+        region.center.longitude = userLocation.coordinate.longitude;
+        mapView.setRegion(region, animated:true)
+    }
+    
     func mapView(mapView: MKMapView, viewForAnnotation annotation: MKAnnotation) -> MKAnnotationView? {
         // if the annotation is a pokemon annotation
         if let annotation = annotation as? pokemonAnnotation {
             // identifier used to identify views which can be reused
             let identifier = "pokemon"
-            var view: MKPinAnnotationView
+            var view: MKAnnotationView
             // if there is a view we can reuse then reuse that
             if let dequeuedView = mapView.dequeueReusableAnnotationViewWithIdentifier(identifier)
-                as? MKPinAnnotationView {
+                as MKAnnotationView! {
                 dequeuedView.annotation = annotation
                 view = dequeuedView
             // else create a new document
             } else {
-                view = MKPinAnnotationView(annotation: annotation, reuseIdentifier: identifier)
+                view = MKAnnotationView(annotation: annotation, reuseIdentifier: identifier)
+                
+                // let the pin show a callout bubble
                 view.canShowCallout = true
-                view.calloutOffset = CGPoint(x: -5, y: 5)
-                // shold set the image but doesnt
-                view.image = UIImage(named: "GenericPokeball")
+                
+                // offset the callout from the pin
+                view.calloutOffset = pinCalloutOffset
+                
+                // set the image of the pin
+                view.image = getPinImage(annotation)
                 view.rightCalloutAccessoryView = UIButton(type: .DetailDisclosure) as UIView
             }
 
@@ -39,4 +51,32 @@ extension ViewController: MKMapViewDelegate {
         }
         return nil
     }
+    
+    func getPinImage(annotation : MKAnnotation) -> UIImage {
+        var pinImage : UIImage?
+        
+        //creates a bitmap image context, used for computing image resizing with the gpu
+        UIGraphicsBeginImageContext(pinImageSize)
+        
+        //set the image based on the type and any further information
+        if(annotation is pokemonAnnotation) {
+            pinImage = UIImage(named: "GenericPokeball")!
+        }
+        
+        // draw the chosen image in a correctly sized rectangle in the current graphics context
+        pinImage!.drawInRect(CGRectMake(0, 0, pinImageSize.width, pinImageSize.height))
+        
+        // close the graphics context and return the image
+        let resizedImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        return resizedImage
+    }
+    
+    
+//    func mapView(mapView: MKMapView?, annotationView view: MKAnnotationView?,
+//                 calloutAccessoryControlTapped control: UIControl?) {
+//        let location = view.annotation as! Artwork
+//        let launchOptions = [MKLaunchOptionsDirectionsModeKey: MKLaunchOptionsDirectionsModeDriving]
+//        location.mapItem().openInMapsWithLaunchOptions(launchOptions)
+//    }
 }
