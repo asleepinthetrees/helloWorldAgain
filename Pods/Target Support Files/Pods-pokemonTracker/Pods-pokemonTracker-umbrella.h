91b22a68013275fba85e48d1b60058a2e2d1cc09
Luke Murray <lmurray.nyc@gmail.com>
2016-07-18 15:32:03 -0400

    pod files now

---
+++ Pods/Target Support Files/Pods-pokemonTracker/Pods-pokemonTracker-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_pokemonTrackerVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_pokemonTrackerVersionString[];
+
---
+++ Pods/Target Support Files/Pods-pokemonTrackerTests/Pods-pokemonTrackerTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_pokemonTrackerTests : NSObject
+@end
+@implementation PodsDummy_Pods_pokemonTrackerTests
+@end
